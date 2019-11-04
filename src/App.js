0f07ca271d422c61fdc798712c40d88baaafce22
PRABHAKAR9107 <02panjiyara@gmail.com>
2019-11-04 09:42:35 -0500

    dat

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 
 import "./App.scss";
 
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 import Navbar from "./components/Navbar/Navbar";
 
 import Tourlist from "./components/Tourlist";
--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -4,6 +4,27 @@ import "./tour.scss";
 
 export default class Tour extends Component {
   render() {
-    return <h1>hello From Tour</h1>;
+    const { id, city, img, name, info } = this.props.tour;
+    const { removeTour } = this.props;
+    return (
+      <article className="tour">
+        <div className="img-container">
+          <img src={img} alt="tour image" />
+          <span className="close-btn">
+            <i className="fas fa-window-close"></i>
+          </span>
+        </div>
+        <div className="tour-about">
+          <h3>{name}</h3>
+          <h4>{city}</h4>
+          <h5>
+            <span>
+              <i className="fas fa-caret-square-down" />
+            </span>
+            {info}
+          </h5>
+        </div>
+      </article>
+    );
   }
 }
